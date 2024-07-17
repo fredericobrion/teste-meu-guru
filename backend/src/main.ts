f6f6a252ca0df8f7c0135d39c8d939c769a8989d
Frederico Brion <fredericoasbrion@gmail.com>
2024-07-17 16:05:18 -0300

    feat: cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  await app.listen(3000);
+  app.enableCors({
+    origin: FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
+  await app.listen(3001);
 }
 bootstrap();
