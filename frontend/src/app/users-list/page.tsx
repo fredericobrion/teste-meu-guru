df854853c7c5e8152a91baaef405a0d5c68b38fe
Frederico Brion <fredericoasbrion@gmail.com>
2024-07-17 18:47:49 -0300

    feat: fetch users

--- frontend/src/app/users-list/page.tsx
+++ frontend/src/app/users-list/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+import { useEffect, useState } from "react";
+import { getTokenCookie } from "../../utils/cookieUtils";
+import { fetchUserData } from "../../utils/api";
+import { useRouter } from "next/navigation";
+
 export default function UsersListPage() {
+  const router = useRouter();
+
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (getTokenCookie()) {
+        try {
+          const response = await fetchUserData();
+          setUsersData(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        router.push("/");
+      }
+    };
+
+    fetch();
+  }, []);
+
+  console.log(usersData);
+
   return <h2>Lista de usuários</h2>;
 }
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,12 +1,31 @@
 import axios from "axios";
+import { getTokenCookie } from "./cookieUtils";
 
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3001";
 
-const api = axios.create({ baseURL: BACKEND_URL, withCredentials: true });
+const axiosInstance = axios.create({
+  baseURL: BACKEND_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getTokenCookie();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export const login = async (email: string, password: string) => {
   try {
-    const response = await api.post('/auth/login', {
+    const response = await axiosInstance.post("/auth/login", {
       email,
       password,
     });
@@ -18,9 +37,19 @@ export const login = async (email: string, password: string) => {
     return token;
   } catch (error) {
     if (error.response.status) {
-      throw new Error('E-mail e/ou senha inválidos');
+      throw new Error("E-mail e/ou senha inválidos");
     } else {
-      throw new Error('Erro ao comunicar com o servidor');
+      throw new Error("Erro ao comunicar com o servidor");
     }
+  }
+};
+
+export const fetchUserData = async () => {
+  try {
+    const response = await axiosInstance.get("/user");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar dados do usuário");
   }
 };
