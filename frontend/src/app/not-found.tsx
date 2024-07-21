cb0333605ea2480de83a4e1356d58a9d795d39d4
Frederico Brion <fredericoasbrion@gmail.com>
2024-07-21 16:48:38 -0300

    feat: transform filter input in form

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,22 +1,26 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Header from "./components/header";
 
 export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-center my-40">
-      <div className="text-center p-6 rounded-lg">
-        <h2 className="text-4xl font-bold text-red-600">Houve um problema</h2>
-        <h3 className="text-xl text-gray-700 mt-2 mb-4">
-          Não conseguimos encontrar a página buscada.
-        </h3>
-        <button
-          onClick={() => router.push('/')}
-          className="bg-purple-500 text-white font-bold py-2 px-4 rounded-md hover:bg-purple-600 hover:scale-105 cursor-pointer mt-4"
-        >
-          Voltar
-        </button>
+    <div>
+        <Header />
+      <div className="flex flex-col items-center justify-center my-40">
+        <div className="text-center p-6 rounded-lg">
+          <h2 className="text-4xl font-bold text-red-600">Houve um problema</h2>
+          <h3 className="text-xl text-gray-700 mt-2 mb-4">
+            Não conseguimos encontrar a página buscada.
+          </h3>
+          <button
+            onClick={() => router.push('/')}
+            className="bg-purple-500 text-white font-bold py-2 px-4 rounded-md hover:bg-purple-600 hover:scale-105 cursor-pointer mt-4"
+          >
+            Voltar
+          </button>
+        </div>
       </div>
     </div>
   );
--- frontend/src/app/users-list/page.tsx
+++ frontend/src/app/users-list/page.tsx
@@ -11,10 +11,9 @@ import {
   ChevronDoubleLeftIcon,
 } from "@heroicons/react/24/outline";
 import Swal from "sweetalert2";
-import { useAppContext } from '../../context/context';
+import { useAppContext } from "../../context/context";
 import Loading from "../components/loading";
-import Header from '../components/header';
-
+import Header from "../components/header";
 
 export default function UsersListPage() {
   const router = useRouter();
@@ -48,7 +47,7 @@ export default function UsersListPage() {
             router.push("/");
           }
         });
-        
+
         setTimeout(() => {
           setLoading(false);
           router.push("/");
@@ -81,9 +80,7 @@ export default function UsersListPage() {
       setLoading(true);
       const response = await fetchUserData(page, limit, filter);
       setTotalUsers(response.total);
-      setUsersData(
-        response.data
-      );
+      setUsersData(response.data);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -95,131 +92,157 @@ export default function UsersListPage() {
     }
   };
 
-  return loading ? <Loading /> : (
+  const handleClear = () => {
+    getUsers(1, itemsPerPage, "")
+    setFilterValue("");
+  };
+
+  return loading ? (
+    <Loading />
+  ) : (
     <div>
       <Header />
-    <div className="container mx-auto p-4">
-      <div className="flex flex-col items-center mb-4">
-        <h2 className="text-2xl font-semibold mb-4">Lista de usuários</h2>
-        <div className="flex items-center justify-between w-full mb-4">
-          <div className="flex items-center space-x-2">
-            <input
-              type="text"
-              placeholder="Filtrar por nome ou e-mail"
-              value={filterValue}
-              onChange={(e) => setFilterValue(e.target.value)}
-              className="border border-gray-300 rounded-md px-2 py-1"
-            />
-            <button
-              onClick={() => getUsers()}
-              className="bg-purple-500 text-white rounded-md px-4 py-1 hover:bg-purple-600"
+      <div className="container mx-auto p-4">
+        <div className="flex flex-col items-center mb-4">
+          <h2 className="text-2xl font-semibold mb-4">Lista de usuários</h2>
+          <div className="flex items-center justify-between w-full mb-4">
+            <form
+              className="flex items-center space-x-2"
+              onSubmit={() => getUsers()}
             >
-              Buscar
-            </button>
+              <input
+                type="text"
+                placeholder="Filtrar por nome ou e-mail"
+                value={filterValue}
+                onChange={(e) => setFilterValue(e.target.value)}
+                className="border border-gray-300 rounded-md px-2 py-1"
+              />
+              <button
+                type="submit"
+                className="bg-purple-500 text-white rounded-md px-4 py-1 hover:bg-purple-600"
+              >
+                Buscar
+              </button>
+              <button
+                onClick={() => handleClear()}
+                className="bg-red-500 text-white rounded-md px-4 py-1 hover:bg-red-600"
+              >
+                Limpar
+              </button>
+            </form>
+            <select
+              value={itemsPerPage}
+              onChange={handleItemsPerPageChange}
+              className="border border-gray-300 rounded-md px-2 py-1"
+            >
+              <option data-testid="select-option" value={5}>
+                5 por página
+              </option>
+              <option data-testid="select-option" value={10}>
+                10 por página
+              </option>
+              <option data-testid="select-option" value={20}>
+                20 por página
+              </option>
+              <option data-testid="select-option" value={30}>
+                30 por página
+              </option>
+              <option data-testid="select-option" value={40}>
+                40 por página
+              </option>
+              <option data-testid="select-option" value={50}>
+                50 por página
+              </option>
+            </select>
           </div>
-          <select
-            value={itemsPerPage}
-            onChange={handleItemsPerPageChange}
-            className="border border-gray-300 rounded-md px-2 py-1"
-          >
-            <option data-testid="select-option" value={5}>5 por página</option>
-            <option data-testid="select-option" value={10}>10 por página</option>
-            <option data-testid="select-option" value={20}>20 por página</option>
-            <option data-testid="select-option" value={30}>30 por página</option>
-            <option data-testid="select-option" value={40}>40 por página</option>
-            <option data-testid="select-option" value={50}>50 por página</option>
-          </select>
+          <p className="mb-4 text-gray-600">
+            Resultados encontrados: {totalUsers}
+          </p>
         </div>
-        <p className="mb-4 text-gray-600">
-          Resultados encontrados: {totalUsers}
-        </p>
+        <table className="min-w-full bg-white border border-gray-200">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="py-2 px-4 border-b w-1/5">E-mail</th>
+              <th className="py-2 px-4 border-b w-1/5">Nome</th>
+              <th className="py-2 px-4 border-b w-1/5">CPF</th>
+              <th className="py-2 px-4 border-b w-1/5">Telefone</th>
+              <th className="py-2 px-4 border-b w-1/10">Cargo</th>
+              <th className="py-2 px-4 border-b w-1/5">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usersData.map((user) => (
+              <TableRow
+                key={user.id}
+                user={user}
+                usersData={usersData}
+                setUsersData={setUsersData}
+                isAdmin={isAdmin}
+              />
+            ))}
+          </tbody>
+        </table>
+        <div className="flex justify-center mt-4">
+          {currentPage > 2 && (
+            <button
+              onClick={() => handlePageChange(1)}
+              className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
+            >
+              <ChevronDoubleLeftIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
+            </button>
+          )}
+          {currentPage > 1 && (
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
+              disabled={currentPage === 1}
+            >
+              <ChevronLeftIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
+            </button>
+          )}
+          {totalPages > 3 && currentPage > 1 && (
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
+            >
+              {currentPage - 1}
+            </button>
+          )}
+          <button
+            onClick={() => handlePageChange(currentPage)}
+            className="px-4 py-2 mx-1 rounded-md border bg-purple-600 text-white hover:bg-purple-700"
+          >
+            {currentPage}
+          </button>
+          {totalPages > 3 && currentPage < totalPages && (
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
+            >
+              {currentPage + 1}
+            </button>
+          )}
+          {currentPage < totalPages && (
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
+              disabled={currentPage === totalPages}
+              data-testid="next-page-btn"
+            >
+              <ChevronRightIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
+            </button>
+          )}
+          {currentPage + 1 < totalPages && (
+            <button
+              onClick={() => handlePageChange(totalPages)}
+              className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
+            >
+              <ChevronDoubleRightIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
+            </button>
+          )}
+        </div>
+        {error && <p>{error}</p>}
       </div>
-      <table className="min-w-full bg-white border border-gray-200">
-        <thead>
-          <tr className="bg-gray-100">
-            <th className="py-2 px-4 border-b w-1/5">E-mail</th>
-            <th className="py-2 px-4 border-b w-1/5">Nome</th>
-            <th className="py-2 px-4 border-b w-1/5">CPF</th>
-            <th className="py-2 px-4 border-b w-1/5">Telefone</th>
-            <th className="py-2 px-4 border-b w-1/10">Cargo</th>
-            <th className="py-2 px-4 border-b w-1/5">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {usersData.map((user) => (
-            <TableRow
-              key={user.id}
-              user={user}
-              usersData={usersData}
-              setUsersData={setUsersData}
-              isAdmin={isAdmin}
-            />
-          ))}
-        </tbody>
-      </table>
-      <div className="flex justify-center mt-4">
-        {currentPage > 2 && (
-          <button
-            onClick={() => handlePageChange(1)}
-            className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
-          >
-            <ChevronDoubleLeftIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
-          </button>
-        )}
-        {currentPage > 1 && (
-          <button
-            onClick={() => handlePageChange(currentPage - 1)}
-            className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
-            disabled={currentPage === 1}
-          >
-            <ChevronLeftIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
-          </button>
-        )}
-        {totalPages > 3 && currentPage > 1 && (
-          <button
-            onClick={() => handlePageChange(currentPage - 1)}
-            className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
-          >
-            {currentPage - 1}
-          </button>
-        )}
-        <button
-          onClick={() => handlePageChange(currentPage)}
-          className="px-4 py-2 mx-1 rounded-md border bg-purple-600 text-white hover:bg-purple-700"
-        >
-          {currentPage}
-        </button>
-        {totalPages > 3 && currentPage < totalPages && (
-          <button
-            onClick={() => handlePageChange(currentPage + 1)}
-            className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
-          >
-            {currentPage + 1}
-          </button>
-        )}
-        {currentPage < totalPages && (
-          <button
-            onClick={() => handlePageChange(currentPage + 1)}
-            className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
-            disabled={currentPage === totalPages}
-            data-testid="next-page-btn"
-          >
-            <ChevronRightIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
-          </button>
-        )}
-        {currentPage + 1 < totalPages && (
-          <button
-            onClick={() => handlePageChange(totalPages)}
-            className="px-4 py-2 mx-1 rounded-md border bg-purple-300 text-white hover:bg-purple-600"
-          >
-            <ChevronDoubleRightIcon className="size-4 text-white hover:text-gray-600 cursor-pointer" />
-          </button>
-        )}
-      </div>
-      {error && (
-        <p>{error}</p>
-      )}
-    </div>
     </div>
   );
 }
